cdfeb61fe10c65b9c42119354775c6884c48e6b9
PavanKalyan-git <pavanindians@gmail.com>
2025-05-04 18:43:43 +0530

    everything deleted

--- models/DishAvailability.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const dishAvailabilitySchema = new mongoose.Schema({
-  dish_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Dish', required: true },
-  restaurant_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant', required: true },
-  price: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('DishAvailability', dishAvailabilitySchema);
--- tests/setup.js
+++
@@ -1,1 +0,0 @@
-process.env.NODE_ENV = 'test';  // Set the environment to 'test' before running the tests
